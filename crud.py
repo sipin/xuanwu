822bc03147ba2f17cd89d7bda8db9c248487cbb1
Cheney <me@chenye.org>
2014-07-10 04:42:58 +0800

    add crud permission to controller

--- crud.py
+++ crud.py
@@ -46,6 +46,63 @@ def fieldElem(field, key):
             return att.value.value
     return ""
 
+def fieldElems(field, key):
+    key = key.lower()
+    ret = []
+    for att in field.annotations:
+        if att.name.value.lower() == key:
+            ret.append(att.value.value)
+    return ret
+
+def permission(name, action, obj):
+    validType = ["User", "Role", "Department", "Organization"]
+    ret = []
+    for p in obj:
+        item = []
+        pis = p.split(",")
+        for pi in pis:
+            pos = "(%s.%s=%s)" % (name, action, pi)
+            nameType = pi.split(":")
+            if len(nameType) != 2:
+                raise Exception("%s.%s is invalid: '%s'" % (name, action, pi))
+            perm = dict(name=nameType[0], type=nameType[1])
+            if perm["type"] not in validType:
+                raise Exception("type not in %s %s" % (validType, pos))
+            if perm["type"] != "Role" and perm["name"] != "Owner":
+                raise Exception("%s name must be Owner %s" % (perm["type"], pos))
+            if perm["type"] == "Role" and perm["name"] == "Owner":
+                raise Exception("Owner can not use Role type %s" % pos)
+            item.append(perm)
+        ret.append(item)
+    return ret
+
+def dealPermission(obj, idField, thrift_idl, labelName):
+    location = "%s.%s" % (os.path.basename(thrift_idl), str(obj.name))
+    obj.crud = dict()
+    obj.create = permission(location, "Create", fieldElems(idField, "Create"))
+    obj.read = permission(location, "Read", fieldElems(idField, "Read"))
+    obj.update = permission(location, "Update", fieldElems(idField, "Update"))
+    obj.delete = permission(location, "Delete", fieldElems(idField, "Delete"))
+    obj.hasUser = len([i for i in obj.fields if str(i.name) == "UsersID"]) > 0
+    if len(obj.create) + len(obj.read) + len(obj.update) + len(obj.delete) > 0:
+        if not obj.hasUser:
+            raise Exception("%s use crud and miss UsersID" % (location))
+    obj.permission = []
+
+    for items in obj.create + obj.read + obj.update + obj.delete:
+        for perm in items:
+            if perm["name"] == "Owner": continue
+            p = dict(name=perm["name"],type=perm["type"],category=labelName)
+            if p["type"] == "Role":
+                p["type"] = ["Role", "User"]
+            else:
+                p["type"] = [p["type"]]
+            p["type"] = ", ".join(["mp.Type." + i for i in p["type"]])
+            obj.permission.append(p)
+    if len(obj.permission) > 0:
+        obj.imports.append("admin/permission")
+        obj.imports.append(("mp", "zfw/models/permission"))
+
 
 def main(thrift_idl):
     source = open(thrift_idl, 'r').read()
@@ -80,6 +137,8 @@ def main(thrift_idl):
             for field in obj.fields:
                 if field.name.value == fieldname:
                     obj.filterFields.append(field)
+
+        dealPermission(obj, idField, thrift_idl, labelName)
 
         # make sure all field in filter fields exists
         if len(filterFields) > len(obj.filterFields):
