d7d104aceb08cb064f48d4e6413caaa222bf5bd0
isombyt <isombyt@isombyt.me>
2014-08-22 14:47:21 +0800

    fix error indent

--- crud.py
+++ crud.py
@@ -34,9 +34,9 @@ def updateController(out_path):
 
 	content = content + ")\n"
 	f = open(out_path + "gen_init.go", "w")
-		fcntl.flock(f, fcntl.LOCK_EX)
+	fcntl.flock(f, fcntl.LOCK_EX)
 	f.write(content)
-		fcntl.flock(f, fcntl.LOCK_UN)
+	fcntl.flock(f, fcntl.LOCK_UN)
 	f.close()
 
 def getControlDir(urlBase):
